0dce48502eb238d038d272c4b84e7e1717141756
iniffur <jamesruffini97@gmail.com>
2023-02-10 17:52:30 +0000

    Initialised Mongoose and connected to MongoDB

---
+++ app.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+
+dotenv.config();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const password = encodeURIComponent(process.env.MONGOOSE_PASSWORD);
+const mongoDB = `mongodb+srv://iniffur:${password}@cluster0.crtcyqm.mongodb.net/movies_database?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+module.exports = app;
