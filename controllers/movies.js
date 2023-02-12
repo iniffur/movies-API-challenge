a5b955f356703524cff3774bcaa777cfbe48fec7
iniffur <jamesruffini97@gmail.com>
2023-02-12 21:43:40 +0000

    Implemented API A - working as expected

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,3 +1,31 @@
 const Movie = require("../models/movie");
 
-exports.getMovies = (req, res) => {};
+exports.getMovies = async (req, res) => {
+  let movies = await Movie.find();
+
+  if (req.query.title) {
+    movies = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(req.query.title.toLowerCase())
+    );
+  }
+  if (req.query.yearOfRelease) {
+    movies = movies.filter(
+      (movie) => movie.yearOfRelease == req.query.yearOfRelease
+    );
+  }
+  if (req.query.genres) {
+    movies = movies.filter((movie) =>
+      movie.genres
+        .map((genre) => genre.toLowerCase())
+        .includes(req.query.genres.toLowerCase())
+    );
+  }
+
+  if (!req.query.title && !req.query.yearOfRelease && !req.query.genres) {
+    return res.status(400).send({ error: "Invalid or no criteria given" });
+  } else if (movies.length === 0) {
+    return res.status(404).send({ error: "Movie not found" });
+  } else {
+    return res.status(200).send(movies);
+  }
+};
