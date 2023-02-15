5afc793ef8486d6516cd28bc4d2f96c6923c06df
iniffur <jamesruffini97@gmail.com>
2023-02-15 15:23:36 +0000

    Added additional error pathways

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,6 +3,14 @@ const movieService = require("../services/movieService");
 const getMovies = async (req, res) => {
   const { title, yearOfRelease, genres } = req.query;
 
+  if (
+    Object.keys(req.query).some(
+      (key) => key !== "title" && key !== "yearOfRelease" && key !== "genres"
+    )
+  ) {
+    return res.status(400).send({ error: "Invalid query parameter" });
+  }
+
   const movies = await movieService.getFilteredMovies(
     title,
     yearOfRelease,
@@ -10,7 +18,7 @@ const getMovies = async (req, res) => {
   );
 
   if (!title && !yearOfRelease && !genres) {
-    return res.status(400).send({ error: "Invalid or no criteria given" });
+    return res.status(400).send({ error: "No criteria given" });
   } else if (movies.length === 0) {
     return res.status(404).send({ error: "Movie not found" });
   } else {
--- controllers/topFiveMoviesByUser.js
+++ controllers/topFiveMoviesByUser.js
@@ -3,24 +3,28 @@ const User = require("../models/user");
 const topFiveMoviesByUserService = require("../services/topFiveMoviesByUserService");
 
 const getTopFiveMoviesByUser = async (req, res) => {
-  if (Object.keys(req.query).length > 0) {
-    return res.status(400).send({ error: "Invalid query" });
-  }
+  try {
+    if (Object.keys(req.query).length > 0) {
+      return res.status(400).send({ error: "Invalid query" });
+    }
 
-  const user = await User.findOne({ _id: req.params.userId });
-  if (!user) {
-    return res.status(404).send({ error: "User not found" });
-  }
+    const user = await User.findOne({ _id: req.params.userId });
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
 
-  const userRatings = await Rating.find({ userId: req.params.userId }).sort({
-    score: -1,
-  });
-  const movies = await topFiveMoviesByUserService.sortMovies(userRatings);
+    const userRatings = await Rating.find({ userId: req.params.userId }).sort({
+      score: -1,
+    });
+    const movies = await topFiveMoviesByUserService.sortMovies(userRatings);
 
-  if (movies.length === 0) {
-    return res.status(404).send({ error: "Movies not found" });
-  } else {
-    return res.status(200).send(movies);
+    if (movies.length === 0) {
+      return res.status(404).send({ error: "Movies not found" });
+    } else {
+      return res.status(200).send(movies);
+    }
+  } catch (error) {
+    res.status(400).send({ error: error.message });
   }
 };
 
