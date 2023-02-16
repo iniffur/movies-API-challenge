f4543d30d781a304903ec6dd4c2965f400808b3a
iniffur <jamesruffini97@gmail.com>
2023-02-16 01:45:13 +0000

    Resolved issue with 400 error on addRating

--- controllers/rating.js
+++ controllers/rating.js
@@ -12,15 +12,17 @@ const addRating = async (req, res) => {
       return res.status(404).send({ error: "User not found" });
     } else if (!movie) {
       return res.status(404).send({ error: "Movie not found" });
-    } else if (!score) {
-      return res.status(400).send({ error: "No rating given" });
+    }
+
+    if (score.length > 1 || score < 1 || score > 5) {
+      throw new Error("Invalid rating inputted");
     }
 
     ratingService.handleRatings(movie, userId, movieId, score);
 
     return res.status(200).send("New rating accepted!");
   } catch (error) {
-    res.status(400).send({ error: "Invalid rating inputted" });
+    res.status(400).send({ error: error.message });
   }
 };
 
